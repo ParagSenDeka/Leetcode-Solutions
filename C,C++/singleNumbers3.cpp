c534089419f8174371486fbbb494e86aed5f5093
ParagSenDeka <patrickstat022@gmail.com>
2024-06-01 17:29:36 +0530

    N

--- C,C++/singleNumbers3.cpp
+++ C,C++/singleNumbers3.cpp
@@ -1,25 +1,32 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> singleNumber(vector<int>& nums) {
+    vector<int> singleNumber(vector<int> &nums)
+    {
         vector<int> result(2, 0);
         int index = 0;
         int size = nums.size();
-        if (nums.size() == 2) {
+        if (nums.size() == 2)
+        {
             return nums;
         };
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++)
+        {
             bool found = false;
-            for (int j = 0; j < size; j++) {
-                if (i != j && nums[i] == nums[j]) {
+            for (int j = 0; j < size; j++)
+            {
+                if (i != j && nums[i] == nums[j])
+                {
                     found = true;
                     break;
                 }
             }
-            if (!found) {
+            if (!found)
+            {
                 result[index++] = nums[i];
                 if (index == 2)
                     break;
@@ -28,11 +35,13 @@ public:
         return result;
     }
 };
-int main(){
-    vector<int>nums={1,2,1,2,4,5};
+int main()
+{
+    vector<int> nums = {1, 2, 1, 2, 4, 5};
     Solution s;
-    vector<int>solution=s.singleNumber(nums);
-    for(int i=0;i<solution.size();i++){
-        cout<<solution[i];
+    vector<int> solution = s.singleNumber(nums);
+    for (int i = 0; i < solution.size(); i++)
+    {
+        cout << solution[i];
     }
 }
