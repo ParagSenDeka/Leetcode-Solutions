9ee4c6666cf1b3fcb30df62bdd684c652a8b62c0
ParagSenDeka <patrickstat022@gmail.com>
2024-06-05 21:03:14 +0530

    N

--- C,C++/findCommonCharacters.cpp
+++ C,C++/findCommonCharacters.cpp
@@ -1,25 +1,32 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<string> commonChars(vector<string>& words) {
+    vector<string> commonChars(vector<string> &words)
+    {
         vector<string> result;
         vector<int> min_freq(26, INT_MAX);
-        for (auto& word : words) {
+        for (auto &word : words)
+        {
             vector<int> freq(26, 0);
-            for (char c : word) {
+            for (char c : word)
+            {
                 freq[c - 'a']++;
             }
-            for (int i = 0; i < 26; ++i) {
+            for (int i = 0; i < 26; ++i)
+            {
                 min_freq[i] = min(min_freq[i], freq[i]);
             }
         }
 
-        for (int i = 0; i < 26; ++i) {
-            while (min_freq[i]-- > 0) {
+        for (int i = 0; i < 26; ++i)
+        {
+            while (min_freq[i]-- > 0)
+            {
                 result.push_back(string(1, i + 'a'));
             }
         }
@@ -27,11 +34,13 @@ public:
     }
 };
 
-int main(){
-    vector<string>word={"helo","bella","ella"};
+int main()
+{
+    vector<string> word = {"helo", "bella", "ella"};
     Solution sol;
-    vector<string>result=sol.commonChars(word);
-    for(auto c:result){
-        cout<<c;
+    vector<string> result = sol.commonChars(word);
+    for (auto c : result)
+    {
+        cout << c;
     }
 }
