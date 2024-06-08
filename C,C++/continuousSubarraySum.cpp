358cf319102f018003bc5de10cf032f82a55b977
ParagSenDeka <patrickstat022@gmail.com>
2024-06-09 00:41:28 +0530

    N

--- C,C++/continuousSubarraySum.cpp
+++ C,C++/continuousSubarraySum.cpp
@@ -23,6 +23,40 @@ public:
     }
 };
 
+// class Solution {
+// public:
+//     bool checkSubarraySum(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         unordered_map<int, int> remainderMap;
+//         int cumulativeSum = 0;
+        
+//         // Step 1: Initialize Remainder Map
+//         remainderMap[0] = -1;
+        
+//         // Step 2: Iterate Through the List
+//         for (int i = 0; i < n; i++) {
+//             // Step 3: Calculate Cumulative Sum
+//             cumulativeSum += nums[i];
+            
+//             // Step 4: Calculate Remainder
+//             int remainder = k == 0 ? cumulativeSum : cumulativeSum % k;
+            
+//             // Step 5: Update Remainder Map
+//             if (remainderMap.count(remainder)) {
+//                 // Check if segment length is at least two
+//                 if (i - remainderMap[remainder] > 1) {
+//                     return true;
+//                 }
+//             } else {
+//                 remainderMap[remainder] = i;
+//             }
+//         }
+        
+//         // Step 6: Return the Result
+//         return false;
+//     }
+// };
+
 int main(){
     vector<int>nums={23,2,6,4,7};
     int k=7;
