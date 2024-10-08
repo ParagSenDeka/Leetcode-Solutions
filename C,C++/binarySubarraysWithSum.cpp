1c283c68f27a8f99870e2b2ba62cc7ac7aba8e15
Parag Deka <96935811+ParagSenDeka@users.noreply.github.com>
2024-10-08 19:06:31 +0530

    N

---
+++ C,C++/binarySubarraysWithSum.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        unordered_map<int, int> prefixSums;
+        int sum = 0;
+        int result = 0;
+        
+        prefixSums[0] = 1;
+        
+        for (int num : nums) {
+            sum += num;
+            if (prefixSums.find(sum - goal) != prefixSums.end()) {
+                result += prefixSums[sum - goal];
+            }
+            prefixSums[sum]++;
+        }
+        
+        return result;
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int>nums={1,0,1,0,1};
+    int goal=2;
+    cout<<sol.numSubarraysWithSum(nums,goal);
+}
