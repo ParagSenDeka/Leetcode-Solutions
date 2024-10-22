a52397afce1161c12aae542f933bc6164362e8d8
Parag Deka <96935811+ParagSenDeka@users.noreply.github.com>
2024-10-22 16:01:07 +0530

    N

---
+++ C,C++/majorityElement.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int candidate=-1;
+        for(auto num:nums){
+            if(count==0){
+                candidate=num;
+            }
+            count+=(candidate==num)?1:-1;
+        }
+        count=0;
+        for(int num:nums){
+            if(num==candidate){
+                count++;
+            }
+        }
+        if (count > nums.size() / 2) {
+        return candidate;
+    } else {
+        return -1; // Indicates no majority element
+    
+    }
+    }
+};
+
+int main(){
+    vector<int>nums={2,2,1,1,1,2,2};
+    Solution sol;
+    cout<<sol.majorityElement(nums);
+}
