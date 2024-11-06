11a0d0334c5a392afa62b4380585a989143d6316
Parag Deka <96935811+ParagSenDeka@users.noreply.github.com>
2024-11-06 10:22:46 +0530

    N

--- C,C++/longestCommonPrefix.cpp
+++ C,C++/longestCommonPrefix.cpp
@@ -7,28 +7,23 @@ class Solution
 public:
     string longestCommonPrefix(vector<string> &strs)
     {
+        if (strs.empty())
+            return "";
+
         string firstString = strs[0];
         string res = "";
+
         for (int i = 0; i < firstString.size(); i++)
         {
-            bool isCommon = true;
             char currentChar = firstString[i];
             for (int j = 1; j < strs.size(); j++)
             {
                 if (i >= strs[j].size() || strs[j][i] != currentChar)
                 {
-                    isCommon = false;
-                    break;
-                }
-                if (isCommon)
-                {
-                    res += currentChar;
-                }
-                else
-                {
-                    break;
+                    return res;
                 }
             }
+            res += currentChar;
         }
         return res;
     }
@@ -37,6 +32,9 @@ public:
 int main()
 {
     vector<string> strs = {"flower", "flow", "flight"};
+    vector<string> strs2 = {"car", "cir"};
     Solution sol;
     cout << sol.longestCommonPrefix(strs);
+    cout << endl;
+    cout << sol.longestCommonPrefix(strs2);
 }
