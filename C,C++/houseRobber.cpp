c583f8ad5029d683fa1dd0356c93698a14d34375
Parag Deka <96935811+ParagSenDeka@users.noreply.github.com>
2024-11-26 14:00:59 +0530

    N

---
+++ C,C++/houseRobber.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int rob(vector<int> &nums)
+    {
+        if (nums.empty())
+            return 0;
+        if (nums.size() == 1)
+            return nums[0];
+
+        int n = nums.size();
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; ++i)
+        {
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp[n - 1];
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> nums = {1, 2, 3, 1};
+    cout << sol.rob(nums) << endl;
+}
