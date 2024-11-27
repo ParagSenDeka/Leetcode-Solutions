4c4d34c77503b078cc612d8e2fecf7ce9ad15d83
Parag Deka <96935811+ParagSenDeka@users.noreply.github.com>
2024-11-27 13:27:20 +0530

    N

--- C,C++/houseRobber.cpp
+++ C,C++/houseRobber.cpp
@@ -5,30 +5,27 @@ using namespace std;
 class Solution
 {
 public:
-    int rob(vector<int> &nums)
-    {
-        if (nums.empty())
+    int rob(vector<int> &nums){
+        if(nums.empty()){
             return 0;
-        if (nums.size() == 1)
+        }
+        if(nums.size()==1){
             return nums[0];
-
-        int n = nums.size();
-        vector<int> dp(n, 0);
+        }
+        int size=nums.size();
+        vector<int> dp(size, 0);
         dp[0] = nums[0];
         dp[1] = max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; ++i)
-        {
+        for (int i = 2; i < size;i++){
             dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
         }
-
-        return dp[n - 1];
+        return dp[size - 1];
     }
 };
 
 int main()
 {
     Solution sol;
-    vector<int> nums = {1, 2, 3, 1};
+    vector<int> nums = {2,1, 3, 1};
     cout << sol.rob(nums) << endl;
 }
