47858e488f762d89f6cc4eee11638c2fd8f9d097
Parag Deka <96935811+ParagSenDeka@users.noreply.github.com>
2025-04-29 21:56:13 +0530

    N

--- C,C++/interleavingString.cpp
+++ C,C++/interleavingString.cpp
@@ -1,23 +1,28 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool isInterleave(string s1, string s2, string s3) {
+    bool isInterleave(string s1, string s2, string s3)
+    {
         int m = s1.size(), n = s2.size(), o = s3.size();
-        if(m+n!=o)
+        if (m + n != o)
             return false;
-        if(m<n)
+        if (m < n)
             return isInterleave(s2, s1, s3);
         vector<bool> dp(n + 1, false);
         dp[0] = true;
-        for (int j = 1; j <= n;++j){
+        for (int j = 1; j <= n; ++j)
+        {
             dp[j] = dp[j - 1] && s1[j - 1] == s3[j - 1];
         }
-        for (int i = 1; i <= m;++i){
+        for (int i = 1; i <= m; ++i)
+        {
             dp[0] = dp[0] && s1[i - 1] == s3[i - 1];
-            for (int j = 1; j <= n;++j){
+            for (int j = 1; j <= n; ++j)
+            {
                 dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]) || (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
             }
         }
@@ -25,8 +30,9 @@ public:
     }
 };
 
-int main(){
-    string s1 = "hello", s2 = "world", s3 = "helloworld";
+int main()
+{
+    string s1 = "hello", s2 = "wo", s3 = "helwolo";
     Solution s;
     cout << s.isInterleave(s1, s2, s3);
 }
